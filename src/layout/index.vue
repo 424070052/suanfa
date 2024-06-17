c42c92219fc179164a83d93a1f728307be5592c6
yofang <1923257976@qq.com>
2024-06-17 12:21:39 +0800

    fix

---
+++ src/layout/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="outContainer">
+    <div id="nav">
+      <router-link to="/beibao" style="text-align: center;">
+        <img src="static/beibao/index.jpg" alt="" class="navImg">
+        <div>背包问题</div>
+      </router-link>
+    </div>
+    <div id="nav">
+      <router-link to="/short" style="text-align: center;">
+        <img src="static/beibao/index.jpg" alt="" class="navImg">
+        <div>最短问题</div>
+      </router-link>
+    </div>
+    <router-view/>
+  </div>
+  
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+import router from "../router";
+export default {
+  name: 'layout',
+  created() {
+    console.log(router)
+
+  }
+}
+</script>
+
+<style>
+.navImg{
+  width: 50px;
+  height: 50px;
+}
+.outContainer{
+  width: 100%;
+  height:100vh;
+  display: flex;
+}
+.container{
+  background-color: #f0f0f0;
+  width: 90%;
+  height: calc(100vh - 84px);
+  margin: 0 auto;
+  overflow-y: scroll;
+}
+#nav{
+  background-color: #aaaaaa;
+  width: 100%;
+  height: 84px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ src/view/beibao/generate.js
@@ -0,0 +1,45 @@
+//生成游戏数据的小游戏
+const list = ['草莓','车厘子','番石榴','橘子','荔枝','榴莲','芒果','猕猴桃','西瓜','香蕉']  //有的水果数量
+const data = {
+  bagSize:0,
+  fruits:[]
+}
+
+export function generateData(difficulty,cb){
+  data.fruits = []
+  // 生成随机的背包大小(100-250)
+  let bagSize = Math.floor(Math.random()*150+100)
+  if(cb) bagSize = cb
+  //有多少难度就有多少水果
+  //给每个水果生成价格和体积,并且不能有一样的,且体积不操过背包大小的10%
+  for(let i=0;i<difficulty;i++){
+    let price = Math.floor(Math.random()*50+1)
+    let volume = Math.floor(Math.random()*bagSize*0.1+1)
+    //水果的数量
+    let quantity = Math.floor(Math.random()*(bagSize/volume)+1)
+    let fruit = {
+      name:list[i],
+      price:price,
+      volume:volume,
+      quantity:quantity,
+      answer:0
+    }
+    data.fruits.push(fruit)
+    if(checkData(i)){
+      i--
+      data.fruits.pop()
+    }
+  }
+  return data
+}
+//测试有没有一样数据的水果
+function checkData(length){
+  let now = data.fruits[length]
+  for(let i=0;i<length;i++){
+    if (now.price === data.fruits[i].price && now.volume === data.fruits[i].volume){
+      return true
+    }
+  }
+  return false
+}
+
